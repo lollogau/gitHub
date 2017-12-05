fed928fda99c2961fdb6619a44163548c9ca3498
gaudio <lorenzo.gaudio@studenti.unipr.it>
2017-12-05 09:37:02 +0100

    Minor changes. MAtrix can take any value of parity check p

--- LDPCclass.m
+++ LDPCclass.m
@@ -18,38 +18,9 @@ classdef LDPCclass < handle
     end
     
     methods
-        % The fucntion takes as input the LDPC matrix (sparse logic) and produces
-        % as output two files, specified after, in form of cells array.
-        % matCn is the filename containing the indices of the check nodes connected to
-        % a specific variable node (for each variable node).
-        % matVn is the filename containing the indices of the variable nodes connected
-        % to a specific check node (for each check node).
-        % Take indices of nonzero elements of ldpc matrix,  save them into a cell
-        % array and then the cell array into a file.
-        % The string select if the function has to save or read the matrices.
-        function LDPC_saveNodesConnections(obj)
-            % Save indices of check nodes for each variable node
-            CN = cell(1, obj.nVN);
-            for i = 1:obj.nVN
-                CN{i} = find(obj.ParityCheckMatrix(:,i))';
-            end
-            save(obj.matCNI, 'CN')
-            
-            % Save indices of variable nodes for each check node
-            VN = cell(1, obj.nCN);
-            for i = 1:obj.nCN
-                VN{i} = find(obj.ParityCheckMatrix(i,:));
-            end
-            save(obj.matVNI, 'VN')
-        end
-        
-        function LDPC_loadNodesConnections(obj)
-            load(obj.matCNI);
-            load(obj.matVNI);
-            obj.CheckNodesIndex = CN;
-            obj.VariableNodesIndex = VN;
-        end
-        
+        %%%%%%% LDPC decoding function %%%%%%%
+        % 1) Input: the object referred to the class; the bit LLR vector
+        % 2) Output: the bit LLR calculated after the belief propagation of the graph; the weighted syndrome
         function [softOutput, wsyn]  = LDPC_decode(obj, bitLLR)
             % Save a local copy of variables of the object to improve speed performance
             ParityCheckMatrix_local = obj.ParityCheckMatrix;
@@ -65,9 +36,9 @@ classdef LDPCclass < handle
                 
                 % Check Nodes Update
                 count = ones(1, obj.nCN);
-                % count1 = ones(1, obj.nCN);
+%                 count1 = ones(1, obj.nCN);
                 sumTot = sum(MessageCheckToVariable_local, 2); % Sum of incoming messages (from check nodes) for each variable node
-                % height = size(MessageVariableToCheck_local);
+%                 height = size(MessageVariableToCheck_local);
                 for i = 1:obj.nVN % For each variable node
                     for j = 1:length(CheckNodesIndex_local{i}) % For each check node connected to the variable node of index i
                         % The message from the variable node to the check node is equal to the sum of messages to the check node minus the
@@ -82,16 +53,24 @@ classdef LDPCclass < handle
                     end
                     % Try to vectorize the inner for
 %                     nCNforVN = length(CheckNodesIndex_local{i});
+%                     v = 1:nCNforVN;
 %                     % ind = sub2ind(size(MessageVariableToCheck_local), indexCN_local(i,1:nCNforVN), count1(indexCN_local(i,1:nCNforVN)));
-%                     ind = indexCN_local(i,1:nCNforVN) + height(1) .* (count1(indexCN_local(i,1:nCNforVN)) - 1);
-%                     MessageVariableToCheck_local(ind) = bitLLR(i) + sumTot(i) - MessageCheckToVariable_local(i,1:nCNforVN);
-%                     count1(indexCN_local(i,1:nCNforVN)) = count1(indexCN_local(i,1:nCNforVN)) + 1;
+%                     ind = indexCN_local(i,v) + height(1) * (count1(indexCN_local(i,v)) - 1);
+%                     MessageVariableToCheck_local(ind) = bitLLR(i) + sumTot(i) - MessageCheckToVariable_local(i,v);
+%                     count1(indexCN_local(i,v)) = count1(indexCN_local(i,v)) + 1;
                 end
                 
                 % Variable Nodes Update
                 count = ones(1,obj.nVN);
+                % NOTE: numerical proble related to the fact that MessageVariableToCheck martix may contains zeros, if there one variable 
+                % node has less connections with respect to the others. In this case, in order to make the algorithm usable in all cases,
+                % we set zero elements in the matrix equal to a large number, e.g. 50, such that the tanh(50 / 2) = 1, and the one is irrelevant
+                % in the product. Thanks to this, it is possible to extend the dimension of MessageVariableToCheck to embrace all possible 
+                % cases, wrt to different parity check matrices, without any problem.
+                MessageVariableToCheck_local(MessageVariableToCheck_local == 0) = 50; % Make the tanh = 1 for the product
                 prodTot = prod(tanh(MessageVariableToCheck_local / 2), 2);
                 for i = 1:obj.nCN
+                    % prodTot1 = prod(tanh(MessageVariableToCheck_local(i, 1:length(VariableNodesIndex_local{i})) / 2), 2);
                     for j = 1:length(VariableNodesIndex_local{i})
                         MessageCheckToVariable_local(indexVN_local(i,j), count(indexVN_local(i,j))) = 2 * atanh(prodTot(i) / tanh(MessageVariableToCheck_local(i,j) / 2));
                         count(indexVN_local(i,j)) = count(indexVN_local(i,j)) + 1;
@@ -124,6 +103,15 @@ classdef LDPCclass < handle
         end
         
         % Initialize the LDPC class
+        % The fucntion takes as input the LDPC matrix (sparse logic) and produces
+        % as output two files, specified after, in form of cells array.
+        % matCn is the filename containing the indices of the check nodes connected to
+        % a specific variable node (for each variable node).
+        % matVn is the filename containing the indices of the variable nodes connected
+        % to a specific check node (for each check node).
+        % Take indices of nonzero elements of ldpc matrix,  save them into a cell
+        % array and then the cell array into a file.
+        % The string select if the function has to save or read the matrices.
         function LDPC_initialization(obj, p, It, cmd)
             
             obj.ParityCheckMatrix = p; % Parity Check Matrix
@@ -160,8 +148,8 @@ classdef LDPCclass < handle
                 obj.VariableNodesIndex = VN;
             end
             
-            obj.MessageCheckToVariable = zeros(obj.nVN, 8); % Message from check to variable nodes
-            obj.MessageVariableToCheck = zeros(obj.nCN, 7); % Message from variable to check nodes
+            obj.MessageCheckToVariable = zeros(obj.nVN, 15); % Message from check to variable nodes
+            obj.MessageVariableToCheck = zeros(obj.nCN, 15); % Message from variable to check nodes
             
             CheckNodesIndex_local = obj.CheckNodesIndex; % Indeces of check nodes saved in a cell array
             VariableNodesIndex_local = obj.VariableNodesIndex; % Indeces of variable nodes saved in a cell array
@@ -181,11 +169,12 @@ classdef LDPCclass < handle
             obj.indexVN = indexVN_local;
         end
         
+        % Reset internal messages of the LDPC graph. Must be called each time a new decoding stage starts. If joint detection/decoding 
+        % is performed, this method must be called before the iterations of the joint detector/decoder.
         function LDPC_reset(obj)
-            obj.MessageCheckToVariable = zeros(obj.nVN, 8); % Message from check to variable nodes
-            obj.MessageVariableToCheck = zeros(obj.nCN, 7); % Message from variable to check nodes
+            obj.MessageCheckToVariable = zeros(obj.nVN, 15); % Message from check to variable nodes
+            obj.MessageVariableToCheck = zeros(obj.nCN, 15); % Message from variable to check nodes
         end
     end
     
 end
-
